dbf7abbba2a251179ab48baa623f9f5a2b4e2382
EZ3C <mboyer980@gmail.com>
2025-06-06 12:58:04 +0200

    adding the colors enum and moving all enums in a single file

---
+++ headers/enums.hpp
@@ -0,0 +1,24 @@
+#pragma once 
+
+enum Face {
+    U,
+    D,
+    R,
+    L,
+    F,
+    B
+};
+
+enum Direction {
+    CLOCK_WISE,
+    ANTI_CLOCK_WISE
+};
+
+enum Color {
+    WHITE,
+    YELLOW,
+    BLUE,
+    GREEN,
+    ORANGE,
+    RED
+};
